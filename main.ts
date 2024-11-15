d2b2e24f8d63e2bcc74b77e37bee139d87bdc8ef
Wan Ahnaf <80872201+SShinbae@users.noreply.github.com>
2024-11-15 08:38:44 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 100) {
+        basic.showIcon(IconNames.Silly)
+        babyState = 0
+    }
+})
+input.onSound(DetectedSound.Loud, function () {
+    radio.sendNumber(1)
+    basic.showIcon(IconNames.StickFigure)
+    babyState = 1
+})
+let babyState = 0
+esp8266.init(SerialPin.P16, SerialPin.P15, BaudRate.BaudRate115200)
+if (esp8266.isESP8266Initialized()) {
+    basic.showIcon(IconNames.Target)
+} else {
+    basic.showIcon(IconNames.Yes)
+}
+esp8266.connectWiFi("YNWA", "liverpool")
+if (esp8266.isWifiConnected()) {
+    basic.showIcon(IconNames.Yes)
+} else {
+    basic.showIcon(IconNames.No)
+}
+input.setSoundThreshold(SoundThreshold.Loud, 200)
+radio.setGroup(1)
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    if (babyState == 1) {
+        esp8266.sendTelegramMessage("7556620551:AAFrgjj9yWPZzzfPE1_8QsfpfTmHvvxcOeM", "-4537034579", "Your Car is baby!")
+    }
+})
